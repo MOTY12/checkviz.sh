96df347ff7f62b4d508f9c53fb782b7fb28be53d
MOTY <50269513+MOTY12@users.noreply.github.com>
2023-09-15 17:19:47 +0100

    add a shell script

---
+++ checkviz.sh
@@ -0,0 +1,14 @@
+#Naviggate to the checkviz staff link directory and run the checkviz script
+
+cd
+
+cd Documents/GitHub/checkviz-staff
+
+# start the react app server
+yarn install 
+
+yarn start
+
+# open the browser to the checkviz staff link
+
+# open http://localhost:3000
